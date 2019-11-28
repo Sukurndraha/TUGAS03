49c1eedd9e0f3396a44aa2e925817c47b70e1f4b
sukurndraha <sukur.ndraha@gmail.com>
2019-11-28 20:57:05 +0700

    Tugas 3

---
+++ gen/com/example/sukurmanndraha_163303030260_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.sukurmanndraha_163303030260_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
